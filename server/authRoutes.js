be220295043cf74ed59911d4ed7de1005138f23f
Sierra <kissellogan10@gmail.com>
2022-10-23 11:23:50 -0400

    changed comments

--- server/authRoutes.js
+++ server/authRoutes.js
@@ -11,7 +11,7 @@ router.get('/login', (req, res) => {
 router.post('/regData', (req, res) => {
     // handle client data, after signed into spotify
     // should (after checks) register user to db and add to timer
-    // needs to be sent on front end, so until then we'll just access test user data to test spotify api
+    // for now don't register to db
     console.log(req);
 });
 
