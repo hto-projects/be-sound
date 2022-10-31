b7876d11beb0d2cf46ea38ded381dba475d66c6a
Sierra <kissellogan10@gmail.com>
2022-10-30 22:01:21 -0400

    added node-fetch, redeemed token

--- server/authRoutes.js
+++ server/authRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import queryString from 'query-string';
+import fetch from 'node-fetch';
 dotenv.config();
 
 const router = express.Router();
@@ -8,6 +9,7 @@ const redirect_uri = 'http://localhost:26103/regData';
 
 router.get('/login', (req, res) => {
     // redirect to login to spotify
+    // add random scope var for security
     res.redirect('https://accounts.spotify.com/authorize?' +
         queryString.stringify({
             response_type: 'code',
@@ -17,12 +19,31 @@ router.get('/login', (req, res) => {
         }));
 });
 
-router.get('/regData', (req, res) => {
+router.get('/regData', async (req, res) => {
     // handle code after signed into spotify
-    // should (after checks) register user to db and add to timer
+    // should (after error checks) register user to db and add to timer
     // for now don't register to db
+    // add random scope check
     // remember to refresh token
-    console.log(req.query.code);
+    const encoded = new Buffer(process.env.SPOTIFY_ID + ':' + process.env.SPOTIFY_SECRET).toString('base64');
+    fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        body: queryString.stringify({
+            grant_type: 'authorization_code',
+            code: req.query.code,
+            redirect_uri: redirect_uri
+        }),
+        headers: {
+            'Authorization': 'Basic ' + encoded,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }).then(async r => {
+        console.log(await r.json());
+    });
 });
+
+function refreshToken(code) {
+    console.log("refresh");
+}
 
 export default router;
