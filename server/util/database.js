f24984a45f2754306461c9c49ead907c1c77ad96
Sierra <kissellogan10@gmail.com>
2022-11-12 15:27:04 -0500

    Fixed caps issue with DB

--- server/util/database.js
+++ server/util/database.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const mongo = new MongoClient(process.env.MONGO_URI);
-const db = mongo.db('BeSound');
 const userCollection = db.collection('users');
 
 export default async () => {
