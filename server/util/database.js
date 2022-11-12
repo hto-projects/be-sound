b30131e871ffb8f1e280e9cc24b3452956f29972
Sierra <kissellogan10@gmail.com>
2022-11-12 15:27:50 -0500

    Created DB Util functions in separate file

--- server/util/database.js
+++ server/util/database.js
@@ -3,10 +3,48 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const mongo = new MongoClient(process.env.MONGO_URI);
+const db = mongo.db('besound');
 const userCollection = db.collection('users');
 
-export default async () => {
-    const result = await userCollection.findOne({});
-    console.log(result);
-    mongo.close();
-}
+export const db_findOne = (requirements) => {
+    try {
+        const result = userCollection.findOne(requirements);
+        return result;
+    }
+    catch (error) {
+        console.warn(error);
+        return null;
+    }
+};
+
+export const db_findAll = (requirements) => {
+    try {
+        const cursor = userCollection.find(requirements);
+        const results = cursor.toArray();
+        return results;
+    }
+    catch (error) {
+        console.warn(error);
+        return null;
+    }
+};
+
+export const db_createUser = async (properties) => {
+    try {
+        const inserted = await userCollection.insertOne(properties);
+        return inserted;
+    } catch (error) {
+        throw (new Error('Failed to create User!'));
+    }
+};
+
+export const db_updateOne = async (document, newProperties) => {
+    try {
+        const result = await userCollection.updateOne(document, newProperties);
+        return result;
+    }
+    catch (error) {
+        console.warn(error);
+        return null;
+    }
+};
