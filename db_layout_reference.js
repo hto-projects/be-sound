765fc7b588407122ffef0539abeb451006c35cb5
Logan Kissel <kissellogan10@gmail.com>
2022-12-12 12:17:38 -0500

    Updated layout reference

--- db_layout_reference.js
+++ db_layout_reference.js
@@ -1,28 +1,32 @@
 export default {
-    // User Collection
-    "users": [
-        {
-            // example user
-            "isPlaying": true || false,
-            // might want to store these encrypted
-            "spotifyData": {
-                "accToken": "whateverTheTokenIs",
-                "refToken": "whateverTheTokenIs",
-            },
-            "notifObj": {
-                "endpoint": "https://pushservice.com/clientinfo",
-                "expirationTime": null,
-                "keys": {
-                    "p256dh": "BGyyVt9FFV…",
-                    "auth": "R9sidzkcdf…"
-                }
-            },
-            "authData": {
-                "username": "TestUser",
-                "hashed_password": "3i245jeg...",
-                "email": 'testuser@notgmail.com',
-                "friends": null,
-            }
-        }
-    ]
+  // User Collection
+  users: [
+    {
+      // example user
+      isPlaying: true || false,
+      // might want to store these encrypted
+      spotifyData: {
+        accToken: "whateverTheTokenIs",
+        refToken: "whateverTheTokenIs",
+      },
+      notifObj: {
+        endpoint: "https://pushservice.com/clientinfo",
+        expirationTime: null,
+        keys: {
+          p256dh: "BGyyVt9FFV…",
+          auth: "R9sidzkcdf…",
+        },
+      },
+      authData: {
+        username: "TestUser",
+        hashed_password: "3i245jeg...",
+        email: "testuser@notgmail.com",
+        friends: null,
+      },
+      currentPost: {
+        songName: "TestSong",
+      },
+    },
+  ],
 };
+
